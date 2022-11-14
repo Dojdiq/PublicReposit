176f767eec5115da60f12eabb9f6f09f4c66ae46
Alexey Borodov <115919027+Dojdiq@users.noreply.github.com>
2022-11-14 09:05:46 +0300

    Cat

---
+++ HomeWork 16.7.1.py
@@ -0,0 +1,8 @@
+class Cat:
+    def __init__(self, name, sex, age):
+        self.name = name
+        self.sex = sex
+        self.age = age
+
+    def __call__(self):
+        return f'\tname: {self.name}, sex: {self.sex}, age: {self.age}'
